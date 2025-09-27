8e1650efedf91b4a4d1c1709077b86b39bada112
arpan0505 <arpanneog05@gmail.com>
2025-09-27 18:52:31 +0530

    conditional chain

---
+++ chains/conditional_chain.py
@@ -0,0 +1,48 @@
+from langchain_openai import ChatOpenAI 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableParallel, RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+parser = StrOutputParser()
+
+class Feedback(BaseModel):
+    category: Literal["positive", "negative"] = Field(description="Give the sentiment category of the feedback")
+    
+parser2 = PydanticOutputParser(pydantic_object=Feedback)    
+
+prompt1 = PromptTemplate(
+    template="Classify the following feedback into one of these categories: positive or negative\n\nText: {feedback}\n {format_instructions}", 
+    input_variables=["feedback"],
+    partial_variables={"format_instructions": parser2.get_format_instructions()}
+)
+
+classify_chain = prompt1 | model | parser2
+
+prompt2 = PromptTemplate(
+    template="Generate a detailed positive response to the following feedback:\n\n{feedback}", 
+    input_variables=["feedback"]
+)
+prompt3 = PromptTemplate(
+    template="Generate a detailed negative response to the following feedback:\n\n{feedback}", 
+    input_variables=["feedback"]
+)
+
+branch_chain = RunnableBranch(
+    (lambda x: x.category == "positive", prompt2 | model | parser),
+    (lambda x: x.category == "negative", prompt3 | model | parser),
+    RunnableLambda(lambda x: 'No proper sentiment detected.')
+)
+
+final_chain = classify_chain | branch_chain
+
+result = final_chain.invoke({'feedback': "The product quality is outstanding and exceeded my expectations!"})
+print(result)
